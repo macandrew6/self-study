ea2a25406c017afbc503b3ffebea30f286fdc6bc
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-18 12:15:07 -0700

    helper fns

--- ruby-syntax/algorithmic_strategies_exercise/02_is_sorted.rb
+++ ruby-syntax/algorithmic_strategies_exercise/02_is_sorted.rb
@@ -2,6 +2,19 @@
 # The method should return true if the elements are in increasing order, false otherwise.
 # Do not use the built-in Array#sort in your solution :)
 
+def is_sorted(arr)
+  asc = false
+  i = 0
+  while i < arr.length - 1
+    if(arr[i] <= arr[i + 1])
+      asc = true
+    else
+      return false
+    end
+    i += 1
+  end
+  return asc
+end
 
 p is_sorted([1, 4, 10, 13, 15])       # => true
 p is_sorted([1, 4, 10, 10, 13, 15])   # => true
