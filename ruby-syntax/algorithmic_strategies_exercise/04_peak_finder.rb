3768b979e6ab61352ec9e2c21d2c5c1460dcd71d
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-18 18:59:23 -0700

    ruby basic algos

--- ruby-syntax/algorithmic_strategies_exercise/04_peak_finder.rb
+++ ruby-syntax/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -3,6 +3,26 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr) 
+  # edge cases if el to the left is nil && current el is less than el to the right << push into array
+  # edge cases if el to the right is nil && current el is less than el to the left << push into array
+  # if el to the left and right < than current el << push into array
+  peaks = []
+
+  i = 0
+  while i < arr.length
+    if i == 0 && arr[i] > arr[i + 1]
+      peaks << arr[i]
+    elsif i == arr.length - 1 && arr[i] > arr[i - 1]
+      peaks << arr[i]
+    elsif arr[i - 1] < arr[i] && arr[i] > arr[i + 1]
+      peaks << arr[i]
+    end
+    i += 1
+  end
+
+  return peaks
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
--- ruby-syntax/algorithmic_strategies_exercise/05_compress_str.rb
+++ ruby-syntax/algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,30 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+  counter = 0 
+  compression = ''
+  i = 0
+  while i < str.length
+    counter += 1
+    if str[i] != str[i + 1] && counter > 1
+      compression += counter.to_s + str[i]
+      counter = 0
+    elsif str[i] != str[i + 1] && counter == 1
+      compression += str[i]
+      counter = 0
+    end
+    i += 1
+  end
+  return compression
+end
+
+=begin
+'3a'
+current el = ''
+counter = 0 + 1 + 1 + 1
+  a a a b
+=end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
