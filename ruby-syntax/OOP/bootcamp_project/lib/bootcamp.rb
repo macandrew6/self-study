08da6802a668605e31159eac96b3d8b39f086786
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-22 16:28:21 -0700

    working bootcamp class

--- ruby-syntax/OOP/bootcamp_project/lib/bootcamp.rb
+++ ruby-syntax/OOP/bootcamp_project/lib/bootcamp.rb
@@ -7,4 +7,60 @@ class Bootcamp
     @students = []
     @grades = Hash.new {|h, v| h[v] = Array.new}
   end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire (teacher)
+    @teachers.push(teacher)
+  end
+
+  def enroll (student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    else
+      return false
+    end
+  end
+
+  def enrolled? (student)
+    @students.include?(student)
+  end
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade (student, grade)
+    if self.enrolled?(student)
+      @grades[student] << grade
+      return true
+    else
+      return false
+    end
+  end
+
+  def num_grades (student)
+    @grades[student].length
+  end
+
+  def average_grade (student)
+    if self.enrolled?(student) && self.num_grades(student) > 0
+      @grades[student].sum / self.num_grades(student)
+    end
+  end
 end
