94cf90038e328806b1751d51be5f0f383b0f8957
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-23 20:34:02 -0700

    monkey patching methods onto ruby array class

--- ruby-syntax/OOP/monkey_patching_project/lib/array.rb
+++ ruby-syntax/OOP/monkey_patching_project/lib/array.rb
@@ -1,4 +1,8 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def span
+    return nil if self.length == 0
+    sorted = self.sort
+    return sorted[-1] - sorted[0]
+  end
 end
