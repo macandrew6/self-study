1abe07af86f162d1e9e0975f43c7f54cc4d7bd56
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-28 21:46:50 -0700

    completes part 1

--- ruby-syntax/OOP/battleship_project/lib/board.rb
+++ ruby-syntax/OOP/battleship_project/lib/board.rb
@@ -1,4 +1,6 @@
 class Board
+  attr_reader :size
+
   def initialize(n)
     grid = Array.new(n) {Array.new(n)}
     (0...n).each do |row|
@@ -6,8 +8,30 @@ class Board
         grid[row][col] = :N
       end
     end
-    
+
     @grid = grid
+    @size = n * n
+  end
+
+  def [](position)
+    row, col = position
+    @grid[row][col]
+  end
+
+  def []=(position, value)
+    row, col = position
+    @grid[row][col] = value
+  end
+
+  def num_ships
+    count = 0
+
+    (0...@grid.length).each do |row|
+      (0...@grid.length).each do |col|
+        count += 1 if @grid[row][col] == :S
+      end
+    end
     
+    count
   end
 end
