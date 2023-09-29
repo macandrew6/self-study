1aca1080c34bd2602373e38b80d3fcb757e2319f
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-28 21:10:52 -0700

    builds out play board

--- ruby-syntax/OOP/battleship_project/lib/board.rb
+++ ruby-syntax/OOP/battleship_project/lib/board.rb
@@ -1,3 +1,13 @@
 class Board
-  
+  def initialize(n)
+    grid = Array.new(n) {Array.new(n)}
+    (0...n).each do |row|
+      (0...n).each do |col|
+        grid[row][col] = :N
+      end
+    end
+    
+    @grid = grid
+    
+  end
 end
