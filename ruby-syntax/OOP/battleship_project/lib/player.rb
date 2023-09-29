f51ae77314f8d2fd46ddd30d1daf098f36f1c5d5
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-29 16:17:40 -0700

    stubs updates

--- ruby-syntax/OOP/battleship_project/lib/player.rb
+++ ruby-syntax/OOP/battleship_project/lib/player.rb
@@ -1,5 +1,6 @@
 class Player
   def get_move
+    #needs to add boundary based on board size
     p 'enter a position with coordinates separated with a space like `4 7`'
     user_input = gets.chomp
     user_input.split(' ').map(&:to_i)
