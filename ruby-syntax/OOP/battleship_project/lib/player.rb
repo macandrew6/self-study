de6fcfc244e17f3b60a106a61e8e4b5eb88d27d6
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-28 23:49:16 -0700

    adds player class to battleship

--- ruby-syntax/OOP/battleship_project/lib/player.rb
+++ ruby-syntax/OOP/battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+  def get_move
+    p 'enter a position with coordinates separated with a space like `4 7`'
+    user_input = gets.chomp
+    user_input.split(' ').map(&:to_i)
+  end
 end
