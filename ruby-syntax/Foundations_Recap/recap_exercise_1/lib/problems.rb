26788a92e337c0905778bdfce1556d9a4e7a41a1
macandrew6 <mr.betterandrewmac@gmail.com>
2023-09-30 22:23:51 -0700

    refactors old solutions

--- ruby-syntax/Foundations_Recap/recap_exercise_1/lib/problems.rb
+++ ruby-syntax/Foundations_Recap/recap_exercise_1/lib/problems.rb
@@ -61,16 +61,6 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-    all_bigrams = []
-    found_bigrams = []
-    
-    i = 0
-    while i < str.length - 1
-        bigram = str[i] + str[i+1]
-        all_bigrams << bigram
-        i += 1
-    end
-
     bigrams.select {|bigram| str.include?(bigram)}
 end
 
