1d3883c96d06abe5a837f2012538a357da45b193
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-01 17:08:03 -0700

    working problem set 3 no rspec

---
+++ ruby-syntax/Foundations_Recap/recap_exercise_3/problems_3.rb
@@ -0,0 +1,11 @@
+# no_dupes?
+# Write a method no_dupes?(arr) that accepts an array as an arg and returns an new 
+# array containing the elements that were not repeated in the array.
+
+def no_dupes?(array)
+  
+end
+
+no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+no_dupes?([true, true, true])            # => []
