66d29d1b482cf221f61202ae073b68cf6375d60a
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-01 17:22:38 -0700

    work through coding algos

--- ruby-syntax/Foundations_Recap/recap_exercise_3/problems_3.rb
+++ ruby-syntax/Foundations_Recap/recap_exercise_3/problems_3.rb
@@ -13,7 +13,24 @@ def no_dupes?(array)
   singles.keys
 end
 
-p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
-p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
-p no_dupes?([true, true, true])            # => []
+# p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+# p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+# p no_dupes?([true, true, true])            # => []
+
+
+# no_consecutive_repeats?
+# Write a method no_consecutive_repeats?(arr) that accepts an array as an arg. The 
+# method should return true if an element never appears consecutively in the array; 
+# it should return false otherwise.
+
+def no_consecutive_repeats?(array)
+  (0...array.length - 1).none? {|i| array[i] == array[i + 1]}
+end
+
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+p no_consecutive_repeats?(['x'])                              # => true
+
 
