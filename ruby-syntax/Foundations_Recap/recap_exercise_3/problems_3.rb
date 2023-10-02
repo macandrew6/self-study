fa7d27a7481258bcc2bacf5899c768656273e2d0
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-01 17:11:35 -0700

    working through problem set 3

--- ruby-syntax/Foundations_Recap/recap_exercise_3/problems_3.rb
+++ ruby-syntax/Foundations_Recap/recap_exercise_3/problems_3.rb
@@ -3,9 +3,17 @@
 # array containing the elements that were not repeated in the array.
 
 def no_dupes?(array)
-  
+  counter = Hash.new(0)
+
+  array.each do |el|
+    counter[el] += 1
+  end
+
+  singles = counter.select {|k, v| v == 1}
+  singles.keys
 end
 
-no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
-no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
-no_dupes?([true, true, true])            # => []
+p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+p no_dupes?([true, true, true])            # => []
+
