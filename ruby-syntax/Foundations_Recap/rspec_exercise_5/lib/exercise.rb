3d26340c9972c8cf6ed0dc638b9c9b445b9aab15
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-04 01:09:16 -0700

    zany_zip problem

--- ruby-syntax/Foundations_Recap/rspec_exercise_5/lib/exercise.rb
+++ ruby-syntax/Foundations_Recap/rspec_exercise_5/lib/exercise.rb
@@ -17,3 +17,29 @@ end
 # p zip(a1, a2)
 # p zip(a3)
 
+
+def prizz_proc(array, prc_1, prc_2)
+  array.select do |el|
+    if prc_1.call(el) == true && prc_2.call(el) == false
+      el
+    elsif prc_1.call(el) == false && prc_2.call(el) == true
+      el
+    end
+  end
+end
+
+
+def zany_zip(*arrs)
+  length = arrs.map(&:length).max
+  
+  (0...length).map do |i|
+    arrs.map {|array| array[i]}
+  end
+end
+
+a1 = ['a', 'b', 'c'] 
+a2 = [1]
+a3 = ['w', 'x', 'y', 'b', 'c']
+# p zany_zip(a1, a2, a3)
+p zany_zip(a1, a2)
+p zany_zip(a3)
