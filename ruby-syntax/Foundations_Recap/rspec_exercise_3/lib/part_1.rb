a4bdeab0865a03e90c3d13d7c6a8a996d912955b
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-03 19:42:16 -0700

    nth_prime problem

---
+++ ruby-syntax/Foundations_Recap/rspec_exercise_3/lib/part_1.rb
@@ -0,0 +1,18 @@
+def is_prime?(num)
+  return false if num < 2
+  (2...num).none? {|n| num % n == 0}
+end
+
+def nth_prime(n)
+  num = 1
+  count = 0
+  
+  while count < n
+    num += 1
+    if is_prime?(num)
+      count += 1
+    end
+  end
+
+  num
+end
