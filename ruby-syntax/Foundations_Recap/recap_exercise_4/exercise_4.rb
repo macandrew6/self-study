5764cb2ece88c574f808985395e7b8861342d681
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-04 21:16:44 -0700

    matrix addition reloaded problem

--- ruby-syntax/Foundations_Recap/recap_exercise_4/exercise_4.rb
+++ ruby-syntax/Foundations_Recap/recap_exercise_4/exercise_4.rb
@@ -216,11 +216,44 @@ def tribonacci_number(n)
   tribonacci_number(n - 3) + tribonacci_number(n - 2) + tribonacci_number(n - 1)
 end
 
-p tribonacci_number(1)  # 1
-p tribonacci_number(2)  # 1
-p tribonacci_number(3)  # 2
-p tribonacci_number(4)  # 4
-p tribonacci_number(5)  # 7
-p tribonacci_number(6)  # 13
-p tribonacci_number(7)  # 24
-p tribonacci_number(11) # 274
+# p tribonacci_number(1)  # 1
+# p tribonacci_number(2)  # 1
+# p tribonacci_number(3)  # 2 
+# p tribonacci_number(4)  # 4
+# p tribonacci_number(5)  # 7
+# p tribonacci_number(6)  # 13
+# p tribonacci_number(7)  # 24
+# p tribonacci_number(11) # 274
+
+
+# matrix_addition_reloaded
+# Write a method matrix_addition_reloaded that accepts any number of matrices as 
+# arguments. The method should return a new matrix representing the sum of the arguments. 
+# Matrix addition can only be performed on matrices of similar dimensions, so if all 
+# of the given matrices do not have the same "height" and "width", then return nil.
+
+def matrix_addition_reloaded(*matricies)
+  matrix = matricies.first
+  height = matrix.length
+  width = matrix[0].length
+  
+  new_matrix = Array.new(height) {[0] * width}
+  matricies.inject(new_matrix) do |acc, curr| 
+    return nil if curr.length != height || curr[0].length != width
+    matrix_addition(acc, curr)
+  end
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+matrix_f = [[2 , 3], [7, 5], [4,  6]]
+
+p matrix_addition_reloaded(matrix_a, matrix_b)              # [[11, 6], [7, 7]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_c)    # [[10, 6], [7, 6]]
+p matrix_addition_reloaded(matrix_e)                        # [[0, 0], [12, 4], [6, 3]]
+p matrix_addition_reloaded(matrix_d, matrix_e)              # [[2, -5], [19, 14], [6, 4]]
+p matrix_addition_reloaded(matrix_a, matrix_b, matrix_e)    # nil
+p matrix_addition_reloaded(matrix_d, matrix_e, matrix_f)    # nil
