bbd22e753aea4417bf31f55b6a820c2664306a3d
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-06 15:34:12 -0700

    prctition_platinum problem

--- ruby-syntax/Foundations_Recap/perilous_procs/exercise_5.rb
+++ ruby-syntax/Foundations_Recap/perilous_procs/exercise_5.rb
@@ -301,7 +301,7 @@ def proctition(array, &prc)
   left_arr.concat(right_arr)
 end
 
-# Examples
+# # Examples
 
 # p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
 # # [4, 7, 1, 3, -5, -10, -2]
@@ -309,5 +309,171 @@ end
 # p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
 # # [8, 6, 10, 7, 3]
 
-# p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# p proctition(['duck', 'cab','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
 # # ["boot", "bug", "boat", "cat", "dog"]
+
+
+# selected_map!
+# Write a method selected_map! that accepts an array and two procs as arguments. The 
+# method should replace the elements that return true when passed into the first proc 
+# with their return values when they are passed into the second proc. This method 
+# should mutate the input array and return nil.
+
+def selected_map! (array, prc_1, prc_2)
+  array.each_with_index do |el, i|
+    if prc_1.call(el) == true
+      array[i] = prc_2.call(el)
+    end
+  end
+
+  nil
+end
+
+# Examples
+
+# is_even = Proc.new { |n| n.even? }
+# is_positive = Proc.new { |n| n > 0 }
+# square = Proc.new { |n| n * n }
+# flip_sign = Proc.new { |n| -n }
+
+# arr_1 = [8, 5, 10, 4]
+# p selected_map!(arr_1, is_even, square)     # nil
+# p arr_1                                     # [64, 5, 100, 16]
+
+# arr_2 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_2, is_even, flip_sign)  # nil
+# p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+# arr_3 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_3, is_positive, square) # nil
+# p arr_3                                     # [-10, 16, 49, 36, -2, -9]
+
+
+# chain_map
+# Write a method that accepts any value and an array of procs as an argument. The 
+# method should return the final result of feeding the value through all of the procs. 
+# For example, if the array contains three procs, then:
+
+# the value is given to the first proc
+# the result of the first proc is given to the second proc
+# the result of the second proc is given to the third proc
+# the result of third proc is the final result
+# Examples
+
+def chain_map(n, procs)
+  value = n
+  procs.each do |prc|
+    value = prc.call(value)
+  end
+  value
+end
+
+# add_5 = Proc.new { |n| n + 5 }
+# half = Proc.new { |n| n / 2.0 }
+# square = Proc.new { |n| n * n }
+
+# p chain_map(25, [add_5, half])          # 15.0
+# p chain_map(25, [half, add_5])          # 17.5
+# p chain_map(25, [add_5, half, square])  # 225
+# p chain_map(4, [square, half])          # 8
+# p chain_map(4, [half, square])          # 4
+
+
+# proc_suffix
+# Write a method proc_suffix that accepts a sentence and a hash as arguments. The 
+# hash contains procs as keys and suffix strings as values. The method should return 
+# a new sentence where each word of the original sentence is appended with a suffix 
+# if the original word returns true when given to the corresponding proc key. If an 
+#   original word returns true for multiple procs, then the suffixes should be appended 
+# in the order that they appear in the input hash.
+
+def proc_suffix(sent, hash)
+  words = sent.split(' ')
+  new_words = []
+  words.each do |word|
+    new_word = word
+    hash.each do |prc, val|
+      new_word += val if prc.call(word) == true
+    end
+    new_words << new_word
+  end
+
+  new_words.join(' ')
+end
+
+# Examples
+
+# contains_a = Proc.new { |w| w.include?('a') }
+# three_letters = Proc.new { |w| w.length == 3 }
+# four_letters = Proc.new { |w| w.length == 4 }
+
+# p proc_suffix('dog cat',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "dogo catlyo"
+
+# p proc_suffix('dog cat',
+#     three_letters => 'o',
+#     contains_a => 'ly'
+# )   # "dogo catoly"
+
+# p proc_suffix('wrong glad cat',
+#     contains_a => 'ly',
+#     three_letters => 'o',
+#     four_letters => 'ing'
+# )   # "wrong gladlying catlyo"
+
+# p proc_suffix('food glad rant dog cat',
+#     four_letters => 'ing',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "fooding gladingly rantingly dogo catlyo"
+
+
+# proctition_platinum
+# Write a method proctition_platinum that accepts an array and any number of additional 
+# procs as arguments. The method should return a hash where the keys correspond to 
+# the number of procs passed in.
+
+# For example, if three procs are passed in, then the hash should have the keys 1, 
+# 2, and 3.
+# The values associated with each key should be an array containing the elements of 
+# the input array that return true when passed into the corresponding proc.
+
+# For example, this means that the array that corresponds to the key 2 should contain 
+# the elements that return true when passed into the second proc.
+# If an element returns true for multiple procs, then it should only be placed into 
+# the array that corresponds to the proc that appears first in the arguments.
+
+def proctition_platinum(array, *prcs)
+  hash = Hash.new # {1: ['when!', 'WHERE!'], 2: ['what]}
+  prcs.each_index {|i| hash[i + 1] = []}
+
+  array.each do |el| #what
+    prcs.each_with_index do |prc, i|
+      if prc.call(el) #what
+        hash[i + 1] << el
+        break
+      end
+    end
+  end
+
+  hash
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
