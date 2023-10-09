514a237b3c1b28e6db45049daca89d2815c3f799
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-08 20:16:03 -0700

    refactoring

--- ruby-syntax/Foundations_Recap/foundation problems/medlling_medley/exercise_6.rb
+++ ruby-syntax/Foundations_Recap/foundation problems/medlling_medley/exercise_6.rb
@@ -229,3 +229,66 @@ less_than_ten = Proc.new { |n| n < 10 }
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
 
 
+# convert_pig_latin
+# Write a method convert_pig_latin that accepts a sentence as an argument. The method 
+# should translate the sentence according to the following rules:
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+# if the word begins with a vowel, simply add 'yay' to the end of the word 
+#   (example: 'eat'->'eatyay')
+# if the word begins with a non-vowel, move all letters that come before the word's 
+# first vowel to the end of the word and add 'ay' (example: 'trash'->'ashtray')
+# Note that if words are capitalized in the original sentence, they should remain 
+# capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+def convert_pig_latin(sent)
+  words = sent.split(' ')
+
+  new_words = words.map do |word|
+    new_word = word.length > 2 ? pig_latinify(word) : word
+    word.capitalize == word ? new_word.capitalize : new_word
+  end
+
+  new_words.join(' ')
+end
+
+def pig_latinify(word)
+  vowels = 'aeiou'
+  return word + 'yay' if vowels.include?(word[0])
+  word.each_char.with_index do |ch, i|
+    if vowels.include?(ch.downcase)
+      return  word[i..-1] + word[0...i] + 'ay'
+    end
+  end
+end
+
+# Examples
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
+
+# reverberate
+# Write a method reverberate that accepts a sentence as an argument. The method should 
+# translate the sentence according to the following rules:
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+# if the word ends with a vowel, simply repeat the word twice (example: 'like'->'likelike')
+# if the word ends with a non-vowel, repeat all letters that come after the word's 
+#   last vowel, including the last vowel itself (example: 'trash'->'trashash')
+# Note that if words are capitalized in the original sentence, they should remain 
+# capitalized in the translated sentence. Vowels are the letters a, e, i, o, u.
+
+
+
+# Examples
+
+# p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
+# p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
+# p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
+# p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
