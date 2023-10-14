c60ca44fcf0fc01ba410c92c3c14ebc8a194a834
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-13 20:35:02 -0700

    finished tic tac toe

---
+++ ruby-syntax/Foundation_Projects/tic_tac_toe_v2/game.rb
@@ -0,0 +1,40 @@
+require './board'
+require './human_player'
+
+class Game
+  def initialize(board_size, player_1_mark, player_2_mark)
+    @player_1 = Human_player.new(player_1_mark)
+    @player_2 = Human_player.new(player_2_mark)
+    @board = Board.new(board_size)
+    @current_player = @player_1
+  end
+
+  def switch_turn
+    if @current_player == @player_1
+      @current_player = @player_2
+    else
+      @current_player = @player_1
+    end
+  end
+
+  def play
+
+    while @board.empty_positions?
+      @board.print
+      position = @current_player.get_position
+      mark = @current_player.mark_value
+      @board.place_mark(position, mark)
+
+      if @board.win?(mark)
+        p "Congratulations! player: #{mark} WINS"
+        return
+      else
+        self.switch_turn
+      end
+    end
+    p "Draw"
+  end
+end
+
+game = Game.new(:X, :O, 4)
+game.play
