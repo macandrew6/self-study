e533466fd84bae7eabfe06965ec90dcd2511028d
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-15 00:29:06 -0700

    adds computer player logic

--- ruby-syntax/Foundation_Projects/tic_tac_toe_v3/computer_player.rb
+++ ruby-syntax/Foundation_Projects/tic_tac_toe_v3/computer_player.rb
@@ -4,20 +4,7 @@ class Computer_player
   end
 
   def get_position(legal_positions)
-    p "Player #{@mark_value} enter your position as two numbers with a space between them: ex.(`row col`) "
-    position = gets.chomp
-
-    pos_nums = position.split(' ')
-
-    if pos_nums.length < 2
-      raise 'Invalid input: missing coordinate'
-    end
-
-    valid_entries = ("0".."1000").to_a
-    pos_nums.each do |num|
-      raise "You've entered something other than an integer"if !valid_entries.include?(num)
-    end
-    
-    pos_nums.map(&:to_i)
+    comp_pick = legal_positions.sample
+    p "Computer players chooses #{comp_pick.to_s}"
   end
 end
--- ruby-syntax/Foundation_Projects/tic_tac_toe_v3/human_player.rb
+++ ruby-syntax/Foundation_Projects/tic_tac_toe_v3/human_player.rb
@@ -14,7 +14,6 @@ class Human_player
       input = gets.chomp.split(' ')
 
       input.each do |num|
-        p "i am here this is #{num}"
         raise "You've entered something other than an integer" if !valid_entries.include?(num)
       end
 
@@ -22,10 +21,6 @@ class Human_player
       
       raise 'Invalid input: not enough inputs' if pos_num.length < 2
       p "#{pos_num} is not a legal position" if !legal_positions.include?(pos_num)
-    end
-    pos_num.each do |num|
-      p "i am here this is #{num}"
-      raise "You've entered something other than an integer" if valid_entries.include?(num)
     end
 
     pos_num
