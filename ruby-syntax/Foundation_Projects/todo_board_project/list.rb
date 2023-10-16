6ffc66da1d5e91d35812f4df457e42e6cf059c1a
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-15 22:57:21 -0700

    adds items to list

--- ruby-syntax/Foundation_Projects/todo_board_project/list.rb
+++ ruby-syntax/Foundation_Projects/todo_board_project/list.rb
@@ -1,7 +1,23 @@
+require './item'
+
 class List
   attr_accessor :label
   def initialize(label)
     @label = label
     @items = []
   end
+
+  def add_item(title, deadline, description='')
+    if Item.valid_date?(deadline)
+      @items << Item.new(title, deadline, description)
+      true
+    else
+      false
+    end
+  end
 end
+
+list = List.new('Chores')
+p list.add_item('watch black bird', '2023-12-03', 'description')
+p list.add_item('watch black bird', '2023-13-03', 'description')
+p list
