96527b8f9c255795c789f4bfa606f83fcac6561c
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-16 18:19:17 -0700

    testing priority items

--- ruby-syntax/Foundation_Projects/todo_board_project/list.rb
+++ ruby-syntax/Foundation_Projects/todo_board_project/list.rb
@@ -25,14 +25,32 @@ class List
   end
 
   def swap(index_1, index_2)
-    @items[index_1], @items[index_2] = @items[index_2], @index[index_1]
+    if valid_index?(index_1) && valid_index?(index_2)
+      @items[index_1], @items[index_2] = @items[index_2], @items[index_1]
+      true
+    else
+      false
+    end
+  end
+
+  def [](index)
+    if valid_index?(index)
+      @items[index]
+    else
+      nil
+    end
+  end
+
+  def priority
+    @items[0]
   end
 end
 
 list = List.new('Chores')
-p list.add_item('watch black bird', '2023-12-03', 'description')
-p list.add_item('watch black bird', '2022-06-03', 'description')
-p list.add_item('watch black bird', '2021-08-03', 'description')
-p list.add_item('watch black bird', '2020-11-06', 'description')
-p list.add_item('watch black bird', '2020-12-06', 'description')
-p list
+p list.add_item('watch black bird1', '2023-12-03', 'description')
+p list.add_item('watch black bird2', '2022-06-03', 'description')
+p list.add_item('watch black bird3', '2021-08-03', 'description')
+p list.add_item('watch black bird4', '2020-11-06', 'description')
+p list.add_item('watch black bird5', '2020-12-06', 'description')
+p list.swap(-1, 3)
+p list.priority
