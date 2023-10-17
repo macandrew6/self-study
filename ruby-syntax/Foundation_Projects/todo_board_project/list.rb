1cee7625ba73ed5ae5e258bb5bd3ab3ccbc36b0a
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-16 22:33:26 -0700

    get commands from user interface

--- ruby-syntax/Foundation_Projects/todo_board_project/list.rb
+++ ruby-syntax/Foundation_Projects/todo_board_project/list.rb
@@ -73,15 +73,46 @@ class List
   def print_priority
     print_full_item(0)
   end
+
+  def up(index, amt = 1)
+    if valid_index?(index)
+      amt.times do 
+        swap(index, index - 1)
+        index -= 1
+        true
+      end
+    else
+      false
+    end
+  end
+
+  def down(index, amt = 1)
+    if valid_index?(index)
+      amt.times do 
+        p "#{index} im running"
+        swap(index, index + 1)
+        index += 1
+        true
+      end
+    else
+      false
+    end
+  end
+
+  def sort_by_date!
+    @items.sort_by! {|item| item.deadline}.reverse!
+  end
 end
 
-list = List.new('Chores')
-p list.add_item('watch black bird1', '2023-12-03', 'description')
-p list.add_item('watch black bird2', '2022-06-03', 'description')
-p list.add_item('watch black bird3', '2021-08-03', 'description')
-p list.add_item('watch black bird4', '2020-11-06', 'description')
-p list.add_item('watch black bird5', '2020-12-06', 'description')
-p list.swap(-1, 3)
-p list.priority
-list.print
-list.print_priority
+# list = List.new('Chores')
+# p list.add_item('watch black bird1', '2023-12-03', 'description')
+# p list.add_item('watch black bird2', '2022-06-03', 'description')
+# p list.add_item('watch black bird3', '2021-08-03', 'description')
+# p list.add_item('watch black bird4', '2020-11-06', 'description')
+# p list.add_item('watch black bird5', '2020-12-06', 'description')
+# list.down(0, 12)
+# # list.up(4,2)
+
+# list.sort_by_date!
+# list.print
+# list.print_priority
---
+++ ruby-syntax/Foundation_Projects/todo_board_project/todo_board.rb
@@ -0,0 +1,63 @@
+require './list'
+
+class Todo_Board
+  def initialize(label)
+    @list = List.new(label)
+  end
+
+  def get_command
+    p 'Make a todo with: mktodo <title> <deadline> <optional description>'
+    p 'Move a todo up with: up <index> <optional amount>'
+    p 'Move a todo down with: down <index> <optional amount>'
+    p 'Swap the position of todo with: swap <index_1> <index_2>'
+    p 'Sort the todos in descending order with: sort'
+    p 'Print the top priority with: priority'
+    p 'Print specific todo with: print <optional index>'
+    p 'Print todos with: print'
+    p 'Exit with: quit'
+
+    print "\nEnter a command: "
+    cmd, *args = gets.chomp.split(' ')
+
+    case cmd
+    when 'mktodo'
+      @list.add_item(*args)
+    when 'up'
+      index, amt = *args
+      @list.up(index.to_i, amt.to_i)
+    when 'down'
+      index, amt = *args
+      @list.down(index.to_i, amt.to_i)
+    when 'swap'
+      index_1, index_2 = *args
+      p *args
+      @list.swap(index_1.to_i, index_2.to_i)
+    when 'sort'
+      @list.sort_by!
+    when 'priority'
+      @list.print_priority
+    when 'print'
+      if args.length > 0
+      index = args[0].to_i
+        @list.print_full_item(index)
+      else
+        @list.print
+      end
+    when 'quit'
+      false
+    else
+      p 'sorry that command is not recognized'
+    end
+    
+    true
+  end
+
+  def run
+    until self.get_command == false
+      self.get_command
+    end
+  end
+end
+
+tb1 = Todo_Board.new('House')
+tb1.run
