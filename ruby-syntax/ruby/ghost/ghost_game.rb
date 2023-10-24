5581dfcb470650fc5c5d16353ae0bf252edc9182
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-23 21:19:32 -0700

    testing update standing fn

--- ruby-syntax/ruby/ghost/ghost_game.rb
+++ ruby-syntax/ruby/ghost/ghost_game.rb
@@ -2,13 +2,15 @@ require "set"
 require_relative "player"
 
 class Game
+  attr_reader :fragment, :dictionary, :losses, :players
+
   ALPHA = ('a'..'z').to_a
   MAX_LOSS_COUNT = 5
 
   def initialize(*players)
     words = File.readlines("dictionary.txt").map(&:chomp)
     @players = players
-    @fragment = ''
+    # @fragment = ''
     @dictionary = Set.new(words)
     @losses = Hash.new {|losses, player| losses[player] = 0}
   end
@@ -26,10 +28,11 @@ class Game
   end
 
   def add_letter(letter)
-    @fragment += letter
+    @fragment << letter
   end
 
   def play_round
+    @fragment = ''
     p 'Welcome!'
     p "Lets play a round of GHOST"
     until round_over?
@@ -50,21 +53,23 @@ class Game
   end
 
   def remaining_players
-    losses.count {|k, v| v < MAX_LOSS_COUNT}
+    @losses.count {|k, v| v < MAX_LOSS_COUNT}
   end
   
   def record(player)
-    count = losses[player]
+    count = @losses[player]
     'GHOST'.slice(0..count)
   end
 
   def take_turn
+    system("clear")
     guess = nil
     until valid_play?(guess)
       guess = current_player.guess
 
-      if !valid_play?(guess)
+      unless valid_play?(guess)
         current_player.alert_invalid_guess(guess)
+        guess = nil
       end
     end
 
@@ -81,25 +86,29 @@ class Game
   end
 
   def round_over?
-    dictionary.include?(fragment)
+    @dictionary.include?(@fragment)
   end
 
   def display_standings
     p "Current standings are: "
-    players.each do |player|
+    @players.each do |player|
       p "#{player.name} has : #{record(player)}"
     end
+
+    sleep(2)
   end
 
   def update_standings
-    "#{previous_player} has spelled #{fragment}"
+    "#{previous_player} has spelled #{@fragment}"
     "#{previous_player} gets a letter"
+    sleep(1)
 
-    if losses[previous_player] == MAX_LOSS_COUNT - 1
-      p "#{previous_player} is eliminated."
+    if @losses[previous_player] == MAX_LOSS_COUNT - 1
+      p "#{previous_player.name} is eliminated."
+      sleep(1)
     end
     
-    losses[previous_player] += 1
+    @losses[previous_player] += 1
     display_standings
   end
 end
