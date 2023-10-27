c79f452d63d38d41e70090f763b9ec03953b62e7
macandrew6 <mr.betterandrewmac@gmail.com>
2023-10-26 22:12:05 -0700

    reads maze.txt

--- ruby-syntax/ruby/maze_solver/maze.rb
+++ ruby-syntax/ruby/maze_solver/maze.rb
@@ -1,7 +1,20 @@
 class Maze
+  attr_reader :map
+
   def initialize(filename)
     @map = load_map(filename)
   end
 
-  
+  def load_map(filename)
+    maze = []
+    File.open(filename).each do |line|
+      chars = line.split("")
+      p chars
+      # maze << chars
+    end
+    # p maze
+  end
 end
+
+maze = Maze.new("maze.txt")
+p maze.load_map("maze.txt")
