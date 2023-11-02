9517044665ea15368e8d000d2b188c2173bda15c
macandrew6 <mr.betterandrewmac@gmail.com>
2023-11-01 21:28:45 -0700

    abstraction of branching paths and current

--- ruby-syntax/ruby/maze_solver/solver.rb
+++ ruby-syntax/ruby/maze_solver/solver.rb
@@ -1,6 +1,12 @@
 class Solver
   def initialize(maze)
     @maze = maze
+    reset_values
+  end
+
+  private
+  
+  def reset_values
     @branching_paths = {}
     @current = @maze.find_start
   end
