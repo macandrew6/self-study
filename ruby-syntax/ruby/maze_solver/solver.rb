f945f46bf5c5021ef28cd5a98250413162512ea9
macandrew6 <mr.betterandrewmac@gmail.com>
2023-11-03 20:17:46 -0700

    find distance from point to end

--- ruby-syntax/ruby/maze_solver/solver.rb
+++ ruby-syntax/ruby/maze_solver/solver.rb
@@ -8,9 +8,11 @@ class Solver
 
   def find_distance(point)
     p_x, p_y = point
+    final_x, final_y = @maze.find_end
+    ((p_x + final_x), (p_y + final_y)).abs
   end
 
-  def find_path(point)
+  def find_path(goal = @maze.find_end)
 
   end
 
@@ -22,7 +24,9 @@ class Solver
   end
 end
 
+path = [[1, 6], [1, 5], [1, 4], [1, 3], [1, 2], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1]]
 filename = ARGV[0] || "maze.txt"
 test_maze = Maze.new(filename)
+test_maze.travel_path(path)
 solver = Solver.new(test_maze)
 p solver
