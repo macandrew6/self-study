a896920ad0fb2dcb406ba7c4277f1735a2e58d0d
macandrew6 <mr.betterandrewmac@gmail.com>
2023-11-23 22:41:52 -0800

    testing populate

--- ruby-syntax/references/memory_puzzle/board.rb
+++ ruby-syntax/references/memory_puzzle/board.rb
@@ -1,6 +1,8 @@
 require_relative "card.rb"
 
 class Board
+  attr_reader :size
+
   def initialize(size = 4)
     @grid = Array.new(size) { Array.new(size) }
     @size = size
@@ -8,14 +10,19 @@ class Board
 
   def populate
     num_pairs = (size**2)/2
-    Card.shuffled_pairs(num_pairs)
+    cards = Card.shuffled_pairs(num_pairs)
+    p grid
   end
 
   def render
 
   end
+
+  private
+  
+  attr_reader :grid
 end
 
 p card = Card.new("A")
 board = Board.new
-p board
+p board.populate
