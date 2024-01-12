1ee1b943ed398211a47b94ee6660f315125a6a63
macandrew6 <mr.betterandrewmac@gmail.com>
2024-01-11 20:23:20 -0800

    initialize matched cards and known cards

--- ruby-syntax/references/memory_puzzle/computer_player.rb
+++ ruby-syntax/references/memory_puzzle/computer_player.rb
@@ -1,7 +1,11 @@
 class ComputerPlayer
+  attr_accessor :previous_guess, :board_size
 
   def initialize(size)
-
+    @board_size = size
+    @matched_cards = {}
+    @known_cards = {}
+    @previous_guess = nil
   end
   
 end
